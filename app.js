42939b4e83bc9f1074527f006390e07b29e27f50
ChunTingChan <justine5528@gmail.com>
2017-07-25 23:03:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: 'af8ab1ac-2fe3-4c43-ae3a-d31c647f85db',
+    appPassword: '0sNLiLVLaG2hROTpbrbg69O'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
